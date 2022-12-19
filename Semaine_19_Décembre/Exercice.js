2ae80ddd29135007cec3e4656ee777bbe6823ccf
rosierludivine <ludivine.rosier38@gmail.com>
2022-12-19 17:23:32 +0100

    exercice non fini

---
+++ Semaine_19_Décembre/Exercice.js
@@ -0,0 +1,55 @@
+//1 Écrivez une fonction qui prend deux nombres comme arguments et renvoie leur somme.
+// let number =2 ;// le nombre est de 2 
+// function sum (number){
+// 	return number + number; // addiction du nombre par lui même 
+// }
+// // execution de la fonction 
+// console.log(sum(number));
+
+// Avec une fonction 
+
+
+//2 Écrivez une fonction qui prend un tableau de nombres et renvoie le plus grand nombre du tableau.
+
+// function ArrayNombers( Nombers){
+//     let nombers =[0];
+//     for (let i = [0]; i <nombers.length ; i++){
+//         if (Nombers [i] > max){//Si nombre dans le tableau est superieur a max 
+//             max = Nombers [i];// alors max = le nombre de tableau 
+//         }
+//     }
+// }
+// const result = ArrayNombers(Nombers);
+// console.log(result);
+
+//3- Écrivez une fonction qui prend une chaîne de caractères et renvoie une nouvelle chaîne de caractères dont toutes les voyelles ont été supprimées.
+
+// let array ="welcome";//creation d'une chaine de caractère 
+// function NewString (LastString){
+//     return LastString.replace( /[eaouiy]/gi, '');// on remplace les voyelles par rien, quels soit en majuscule ou en miniscules et pas que le premier 
+// }
+// const result= NewString(array);// on mets la nouvelle valeur danzs une constante 
+// console.log(result);// on affiche le resultat 
+
+// // on execute le  fichier 
+
+// 4-Écrivez une fonction qui prend un tableau de chaînes de caractères et renvoie un tableau de chaînes de caractères triées par ordre alphabétique.
+
+// let arrayString = [ "salut","bienvenue", "Yoyo", "Abricot"];// Création du tableau de chaine de caractère
+
+// function Order (arrayString){
+//     arrayString.sort(); // trier le tableau 
+//    return arrayString;// returner le tableau 
+// }
+// const result= Order(arrayString) // mettre le resultat dans une nouvelle variable 
+// console.log(result);// afficher le resultat 
+
+// 7 Écrivez une fonction qui prend un tableau de nombres et renvoie un tableau de nombres triés par ordre décroissant.
+let Numbers = [3,10,6,42,7];
+function OrderNumbers (Numbers){
+
+   Numbers.reverse();
+   return Numbers;
+}
+const result= OrderNumbers(Numbers); 
+console.log(result);
--- Semaine_19_Décembre/index.js
+++ Semaine_19_Décembre/index.js
@@ -1,1 +1,48 @@
-console.log("Hello Word");
+let arrayNumber = [12,73,99,22,11];
+function nombreMax (number){
+    let number = [0];
+    for ( let i = 0; i < number.length, i++;){// pour i =0 ; 0 < longueur du tableau; i +1
+        if (number [i] > max){//Si nombre dans le tableau est superieur a max 
+            max = number [i];// alors max = le nombre de tableau 
+        }
+    }
+}
+const result = nombreMax(arrayNumber);
+console.log(result);
+
+// correction 
+//Fonction permettant de parcourrir le tableau et sortir le plus grand nombre 
+
+// function getMaxNomber (numbers) {
+//     let max= numbers= [0]; // on declare une variable max on l'initialise avec numbers [0]
+//     // on parcourt le tableau numbers avec la boucle for
+//     for (let i = 0; i< numbers.length, i++;) {
+//         if (numbers[i] > max){
+//             max = numbers[i];
+//         }
+
+//     }
+//     return max; 
+// }
+
+// //Pareil en fléché 
+// const getMaxNomber= (number) => {
+// 	let max= numbers= [0]; // on declare une variable max on l'initialise avec numbers [0]
+//     // on parcourt le tableau numbers avec la boucle for
+//     for (let i = 0; i< numbers.length, i++;) {
+//         if (numbers[i] > max){
+//             max = numbers[i];
+//         }
+
+
+//     }
+//     return max; 
+// }
+
+// // Correction bryan 
+
+
+// const someOtherArray = [2,4,6,9];
+//let bigInt = Math.max(...someOtherArray);
+//console.log(bigInt);
+
